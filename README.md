6df4fff03a55f1e3435d11196376a6885902801e
Maya N <nettathm@gmail.com>
2023-09-14 17:41:13 -0600

    added list of questions and got them to return as a string in template.md

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# readme-generator
+{"title":"maya"}
--- index.js
+++ index.js
@@ -1,10 +1,74 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application - DONE
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+  // list of questions
+  .prompt([
+    {
+      type: "input",
+      message: "Enter the title of your project",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Enter the description of your project",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Enter the table of contents of your project",
+      name: "table of contents",
+    },
+    {
+      type: "input",
+      message: "Enter the instructions for installation",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Enter the usage",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Enter the license information",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Enter any contributors",
+      name: "contributors",
+    },
+    {
+      type: "input",
+      message: "Enter any tests",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "Enter an questions",
+      name: "questions",
+    },
+  ])
+  // return the data when we get a success
+  .then((data) => {
+    console.log(data);
+
+    // format data into a string
+    const readme = JSON.stringify(data);
+
+    // TODO: Create a function to write README file
+    fs.writeFile("template.md", readme, (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Success");
+      }
+    });
+  });
 
 // TODO: Create a function to initialize app
 function init() {}
